1734c71399bbe06fd0b8c01d76949cb7d1187b13
David Radley <david_radley@uk.ibm.com>
2022-10-21 09:18:59 +0100

    git62 merge head
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- src/main/java/org/odpi/egeria/connectors/hms/eventmapper/HMSOMRSRepositoryEventMapper.java
+++ src/main/java/org/odpi/egeria/connectors/hms/eventmapper/HMSOMRSRepositoryEventMapper.java
@@ -17,9 +17,6 @@ import org.odpi.egeria.connectors.hms.helpers.SupportedTypes;
 import org.odpi.egeria.connectors.hms.helpers.ExceptionHelper;
 import org.odpi.openmetadata.adapters.repositoryservices.caching.repository.CachedRepositoryAccessor;
 import org.odpi.openmetadata.adapters.repositoryservices.caching.repositoryconnector.CachingOMRSRepositoryProxyConnector;
-//org.odpi.openmetadata.adapters.repositoryservices.caching.repository
-//import org.odpi.egeria.connectors.hms.repository.CachedRepositoryAccessor;
-//import org.odpi.egeria.connectors.hms.repositoryconnector.CachingOMRSRepositoryProxyConnector;
 import org.odpi.openmetadata.frameworks.connectors.ffdc.ConnectorCheckedException;
 import org.odpi.openmetadata.frameworks.connectors.properties.EndpointProperties;
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.OMRSMetadataCollection;
