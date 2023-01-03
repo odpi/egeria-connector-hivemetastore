58aac8e1d0c4211c68590e8f442605b431325fe5
David Radley <david_radley@uk.ibm.com>
2023-01-03 12:19:50 +0000

    git79 fix script
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- utilities/createIBMHMS.sh
+++ utilities/createIBMHMS.sh
@@ -8,48 +8,55 @@
 #  -  there is a working Kafka on the system
 #  -  there is a folder in the home directory called a egeria, which has a built version of Egeria. See https://egeria-project.org/education/egeria-dojo/developer/overview/?h=building+egeria for information on this.
 # The script
-#  -  git clones the egeria hive connector code
-#  -  downloads the IBM jar
+#  - git clones the egeria hive connector code
+#  - downloads the IBM jar
 #  - amends the build file in a temporary folder
 #  - builds and builds the connector knitting in the IBM jar file
 #  - there should now be an Egeria deployment in folder  ~/ibm-HMS/testplatform which is started
 
 
-# Egeria version
+# Egeria version - amend to match the version of Egeria in ~/egeria
 version=3.15
+temp_folder=ibm-HMS
 
-cd ~
-rm -rf ibm-HMS
-mkdir ibm-HMS
-cd ibm-HMS
+# uncomment next line to attempt to kill existing processes using the port.
+# kill -9 $(lsof -ti:9443)
+
+
+cd ~ || exit
+# remove temp folder if it exists
+if [ -d "${temp_folder}" ]; then
+  rm -rf ${temp_folder}
+fi
+# create new temp folder
+mkdir ${temp_folder}
+
+cd ${temp_folder} || exit
 mkdir src
-cd src
+cd src || exit
 git clone  https://github.com/odpi/egeria-connector-hivemetastore
 cd egeria-connector-hivemetastore
 
 mkdir libs
-cd libs
+cd libs || exit
 curl https://us.sql-query.cloud.ibm.com/download/catalog/hive-metastore-standalone-client-3.1.2-sqlquery.jar -o ./hive-metastore-standalone-client-3.1.2-sqlquery.jar
-cd ..
+cd ~/${temp_folder}/src/egeria-connector-hivemetastore || exit
 mv build.gradle build.gradle.org
-cat build.gradle.org  | sed "/hive:hive-standalone-metastore/ s= 'org.apache.hive:hive-standalone-metastore:3.1.3'=(files('libs/hive-metastore-standalone-client-3.1.2-sqlquery.jar'))=" >build.gradle.org2
-
-cat build.gradle.org2  | sed "/Standard version/ s=Standard=IBM=" >build.gradle
+sed "/hive:hive-standalone-metastore/ s= 'org.apache.hive:hive-standalone-metastore:3.1.3'=(files('libs/hive-metastore-standalone-client-3.1.2-sqlquery.jar'))=" build.gradle.org>build.gradle.org2
+sed "/Standard version/ s=Standard=IBM=" build.gradle.org2 >build.gradle
 rm build.gradle.org
 rm build.gradle.org2
 ./gradlew clean build
-cd ~
-cd ibm-HMS
+cd ~/${temp_folder} || exit
 mkdir testplatform
-cd testplatform
+cd testplatform || exit
 mkdir lib
 
 # copy over server chassis
-
-cp ../../egeria/open-metadata-distribution/open-metadata-assemblies/target/egeria-${version}-SNAPSHOT-distribution/egeria-omag-${version}-SNAPSHOT/server/server-chassis-spring-${version}-SNAPSHOT.jar .
+cp ~/egeria/open-metadata-implementation/server-chassis/server-chassis-spring/build/libs/server-chassis-spring-${version}-SNAPSHOT.jar .
 
 # copy over trust store
-cp ../../egeria/open-metadata-implementation/server-chassis/server-chassis-spring/src/main/resources/truststore.p12 .
+cp ~/egeria/open-metadata-implementation/server-chassis/server-chassis-spring/src/main/resources/truststore.p12 .
 
 # copy over connector jar
 cp ../src/egeria-connector-hivemetastore/build/libs/egeria-connector-hivemetastore-1.0-SNAPSHOT-jar-with-dependencies.jar lib
